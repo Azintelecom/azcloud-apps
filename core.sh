75d753c16c25bf13fd4464a9c2514ebe5248cde3
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-04-08 17:26:56 +0400

    logging; fw fixes

--- core.sh
+++ core.sh
@@ -2,4 +2,4 @@
 
 APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-sudo bash "${APP_DIR}/${APP_PATH}"/app.sh  
+sudo bash "${APP_DIR}/${APP_PATH}"/app.sh 2>&1 >> /tmp/azcloud-apps/"${APP_PATH##*/}".log
