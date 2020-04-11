8b5f0d92b2601d52dbff1df0a2faf861289afaed
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-04-11 22:25:30 +0400

    fix

--- core.sh
+++ core.sh
@@ -3,4 +3,4 @@
 APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 APP_PROXY="$(cat $APP_DIR/.proxy)"
-export HTTP_PROXY="${APP_PROXY}" bash -x "${APP_DIR}/${APP_PATH}"/app.sh >> /tmp/azcloud-apps/"${APP_PATH##*/}".log 2>&1
+export PROXY="${APP_PROXY}"; bash -x "${APP_DIR}/${APP_PATH}"/app.sh >> /tmp/azcloud-apps/"${APP_PATH##*/}".log 2>&1
