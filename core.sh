f786c70873e0edf92589dbf0109ca88ed8fde7cf
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-05-26 13:26:14 +0400

    minifix

--- core.sh
+++ core.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
-APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
 APP_PROXY="$(cat $APP_DIR/.proxy)"
 export PROXY="${APP_PROXY}"; bash -x "${APP_DIR}/${APP_PATH}"/app.sh >> /tmp/azcloud-apps/"${APP_PATH##*/}".log 2>&1
