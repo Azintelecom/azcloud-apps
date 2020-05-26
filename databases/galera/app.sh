82da43e0fa0d9ccf1f0755ca363537c93b6cae79
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-05-27 02:00:00 +0400

    galera: fix in identify self when joining cluster

--- databases/galera/app.sh
+++ databases/galera/app.sh
@@ -19,7 +19,6 @@ _unset_proxy()
   unset HTTPS_PROXY
 }
 
-
 fatal()
 {
   echo "ERROR: $*" >&2
@@ -196,7 +195,7 @@ _start_mariadb_on_all_nodes()
 
   local nodes; nodes=($(grep "$play_id" /etc/hosts | awk '{print $NF}'))
   for node in ${nodes[@]}; do
-    if ! hostname -s | grep -q database1; then
+    if [ "$node" != database1 ]; then
       _run_on_node "$node" "bash /tmp/azcloud-apps/databases/galera/join.sh &"
     fi
   done
