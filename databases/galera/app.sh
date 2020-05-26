76153c0c62c5c1e502e44367411d0872776db976
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-05-26 22:41:05 +0400

    galera: removed sudo(we running under root); mariadb: configuration restored

--- databases/galera/app.sh
+++ databases/galera/app.sh
@@ -34,45 +34,61 @@ _get_nodes_addresses()
   echo "${addresses[@]}"
 }
 
+_setup_firewall_and_selinux()
+{
+    if systemctl is-active firewalld | grep -q active; then
+      firewall-cmd --permanent --zone=public --add-port=3306/tcp
+      firewall-cmd --permanent --zone=public --add-port=4567/tcp
+      firewall-cmd --permanent --zone=public --add-port=4568/tcp
+      firewall-cmd --permanent --zone=public --add-port=4444/tcp
+      firewall-cmd --permanent --zone=public --add-port=4567/udp
+    elif command -v iptables >& /dev/null; then
+      iptables -I INPUT -p tcp --dport 3306 -m comment --comment 'added by azcloud-apps' -m state --state NEW -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4567 -m comment --comment 'added by azcloud-apps' -m state --state NEW -j ACCEPT 
+      iptables -I INPUT -p tcp --dport 4444 -m comment --comment 'added by azcloud-apps' -m state --state NEW -j ACCEPT
+      iptables -I INPUT -p tcp --dport 3306 -m comment --comment 'added by azcloud-apps' -m state --state NEW -j ACCEPT
+      iptables -I INPUT -p udp --dport 4567 -m comment --comment 'added by azcloud-apps' -m state --state NEW -j ACCEPT
+   fi
+
+   # set domain to permissive mode
+   semanage permissive -a mysqld_t
+}
+
 _setup_galera_storage()
 {
   local disks; disks=($(lsblk | grep sd[b-z] | awk '{print $1}' | sed 's@^@/dev/@g'))
   [ -z "$disks" ] && { echo "no disk found"; return 1; }
-  sudo vgcreate galera_storage ${disks[*]}
-  sudo lvcreate -n mariadb -l 100%VG galera_storage
-  sudo mkfs.ext4 /dev/galera_storage/mariadb 
-  sudo mkdir -p /var/lib/mysql
-  sudo mount /dev/galera_storage/mariadb /var/lib/mysql
+   vgcreate galera_storage ${disks[*]}
+   lvcreate -n mariadb -l 100%VG galera_storage
+   mkfs.ext4 /dev/galera_storage/mariadb 
+   mkdir -p /var/lib/mysql
+   mount /dev/galera_storage/mariadb /var/lib/mysql
 }
 
 _install_galera_centos()
 {
-  local temp_config; temp_config="$(mktemp)"
-
-  cat <<EOF > "$temp_config"
+  cat <<EOF > /etc/yum.repos.d/mariadb.repo
 [mariadb]
 name = MariaDB
 baseurl = http://yum.mariadb.org/10.4/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
-  sudo mv "$temp_config" /etc/yum.repos.d/mariadb.repo
-  sudo -E yum makecache --disablerepo='*' --enablerepo='mariadb'
+  yum makecache --disablerepo='*' --enablerepo='mariadb'
   _set_proxy
-  sudo -E yum install -y MariaDB-server MariaDB-client rsync policycoreutils-python
+  yum install -y MariaDB-server MariaDB-client rsync policycoreutils-python
   _unset_proxy
-  sudo systemctl enable --now mariadb
+  systemctl enable --now mariadb
 }
 
 _set_galera_config()
 {
-  local temp_config; temp_config="$(mktemp)"
   local nodes; nodes="$(_get_nodes_addresses | sed 's@ @,@g')"
   local cluster_name; cluster_name="$(hostname -s | awk -F- '{print $(NF-2)}')"
   local hostname; hostname="$(hostname -s)"
   local address; address="$(ip addr sh ens192 | awk '/inet /{print $2}' | cut -d/ -f1)"
 
-  cat <<EOF > "$temp_config"
+  cat <<EOF > /etc/my.cnf.d/galera.cnf
 [mysqld]
 binlog_format=ROW
 default-storage-engine=innodb
@@ -95,11 +111,11 @@ wsrep_node_address="$address"
 wsrep_node_name="$hostname"
 EOF
 
-  sudo mv "$temp_config" /etc/my.cnf.d/galera.cnf
 }
 
 main()
 {
+  _setup_firewall_and_selinux
   _setup_galera_storage
   _install_galera_centos
   _set_galera_config
