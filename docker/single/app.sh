d5a7a1e6f9db6cd6d8bd1b01732c4e7d1e7b4749
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-05-27 18:52:33 +0400

    docker: add users with id >= 1000 to docker group

--- docker/single/app.sh
+++ docker/single/app.sh
@@ -26,11 +26,27 @@ _install_docker()
   curl -fsSL https://get.docker.com/ | sh
 }
 
+_setup_users_to_use_docker()
+{
+  for user in $(cut -d: -f1,3 /etc/passwd | sort -n -t: -k2,2); do 
+    if [ "${user##*:}" -ge 1000 ]; then
+      usermod -aG docker "${user%%:*}"
+    fi
+  done
+}
+
+_start_docker()
+{
+  systemctl enable --now docker
+}
+
 main()
 {
   _setup_firewall_and_selinux
   _setup_docker_repo
   _install_docker
+  _setup_users_to_use_docker
+  _start_docker
 }
 
 main
