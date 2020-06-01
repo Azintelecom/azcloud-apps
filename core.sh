f5949a808c62217e654bace7dae60d1fcad052fb
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-06-01 12:50:07 +0400

    CHANGES: rename app.sh to main; split main file; laod_modules in core.sh

--- core.sh
+++ core.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
-APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
-APP_PROXY="$(cat $APP_DIR/.proxy)"
-export PROXY="${APP_PROXY}"; bash -x "${APP_DIR}/${APP_PATH}"/app.sh >> /tmp/azcloud-apps/"${APP_PATH##*/}".log 2>&1
+load_module()
+{
+  local app_path; app_path="$1"
+  find "$app_path" -type f ! -name \*main\* -a ! -name \*.sh\* \
+    | while read -r file; do source "$file"; done
+}
+
+main()
+{
+  APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
+  APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
+  APP_PROXY="$(cat $APP_DIR/.proxy)"
+  load_module "${APP_DIR}"/"${APP_PATH}"
+  export PROXY="${APP_PROXY}"
+  bash -x "${APP_DIR}"/"${APP_PATH}"/main >> /tmp/azcloud-apps/app.log 2>&1
+}
+
+main "$@"
