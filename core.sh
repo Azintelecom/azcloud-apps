04c4c3e710fd1cdaa093a590cc52276b8fc5a40c
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-06-01 21:02:38 +0400

    core: fixes in module initializing

--- core.sh
+++ core.sh
@@ -3,18 +3,21 @@
 load_module()
 {
   local app_path; app_path="$1"
-  find "$app_path" -type f ! -name \*main\* -a ! -name \*.sh\* \
-    | while read -r file; do source "$file"; done
+  for module in $(find "$app_path" -type f ! -name \*.sh\*); do
+    source $module
+  done
 }
 
 main()
 {
-  APP_PATH="$1"; [ -z $APP_PATH ] && { echo "app not found"; exit 1; }
-  APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
-  APP_PROXY="$(cat $APP_DIR/.proxy)"
-  load_module "${APP_DIR}"/"${APP_PATH}"
-  export PROXY="${APP_PROXY}"
-  bash -x "${APP_DIR}"/"${APP_PATH}"/main >> /tmp/azcloud-apps/app.log 2>&1
+  local app_path app_dir app_proxy run_module
+  app_path="$1"; [ -z $app_path ] && { echo "app not found"; exit 1; }
+  app_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
+  app_proxy="$(cat $app_dir/.proxy)"
+  export PROXY="${app_proxy}"
+  load_module "${app_dir}"/"${app_path}"
+  run_module=${app_path////_}; run_module=${run_module//+/_}
+  azapps_$run_module >> /tmp/azcloud-apps/app.log 2>&1
 }
 
 main "$@"
