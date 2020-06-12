65b46548cd9f1db252ea0848326fc1fa6c284e88
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-06-12 13:36:34 +0400

    postgresql/master+slave: first attempts

--- core.sh
+++ core.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+fatal()
+{
+  echo "ERROR: $*" >&2
+  exit 1
+}
+
 load_module()
 {
   local app_path; app_path="$1"
@@ -12,13 +18,13 @@ main()
 {
   set -x
   local app_path app_dir app_proxy run_module
-  app_path="$1"; [ -z $app_path ] && { echo "app not found"; exit 1; }
+  app_path="$1"; [ -z "$app_path" ] && { echo "app not found"; exit 1; }
   app_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
-  app_proxy="$(cat $app_dir/.proxy)"
+  app_proxy="$(cat "$app_dir"/.proxy)"
   export PROXY="${app_proxy}"
   load_module "${app_dir}"/"${app_path}"
   run_module=${app_path////_}; run_module=${run_module//+/_}
-  azapps_$run_module >> /tmp/azcloud-apps/app.log 2>&1
+  azapps_"$run_module" >> /tmp/azcloud-apps/app.log 2>&1
 }
 
 main "$@"
