90d8144265fe5ec31f698056b4fc499cbdc9f463
Eldar Valibayov <Eldar.Valibayov@exasol.com>
2020-06-30 14:31:49 +0400

    core: minfix

--- core.sh
+++ core.sh
@@ -3,8 +3,10 @@
 load_module()
 {
   local app_path; app_path="$1"
+  # shellcheck disable=SC2044
   for module in $(find "$app_path" -type f ! -name \*.sh\*); do
-    source $module
+    # shellcheck disable=SC1090
+    source "$module"
   done
 }
 
@@ -16,11 +18,12 @@ main()
   app_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >& /dev/null && pwd )"
   app_proxy="$(cat "$app_dir"/.proxy)"
   export PROXY="${app_proxy}"
+  load_module "${app_dir}"/_variables
   load_module "${app_dir}"/_functions
   load_module "${app_dir}"/"${app_path}"
   install_dependencies || fatal "Could not install dependencies"
   run_module=${app_path////_}; run_module=${run_module//+/_}
-  azapps_"$run_module" >> /tmp/azcloud-apps/app.log 2>&1
+  azapps_"$run_module" >> "$AZCLOUD_APPS_HOME"/app.log 2>&1
 }
 
 main "$@"
